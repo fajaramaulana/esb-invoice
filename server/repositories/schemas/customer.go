c6cff3c6f53df473f4cc87e95667f344900bee84
fajaramaulana <fajaragusmaulana@gmail.com>
2023-12-08 23:18:44 +0700

    schema customers

--- server/repositories/schemas/customer.go
+++ server/repositories/schemas/customer.go
@@ -1,16 +1,19 @@
 package schemas
 
-import "time"
+import (
+	"time"
 
-// Customer represents the
+	"gorm.io/gorm"
+)
+
 type Customer struct {
-	Id        int       `gorm:"primaryKey;autoIncrement" json:"id"`
-	Name      string    `gorm:"not null" json:"name"`
-	Email     string    `gorm:"not null;unique" json:"email" gorm:"index"`
-	Address   string    `json:"address"`
-	CreatedAt time.Time `json:"created_at" gorm:"index"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"index"`
-	UserID    uint      `json:"user_id"`
-	User      User      `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"user"`
-	Invoices  []Invoice `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"invoices"`
+	ID        int            `gorm:"primaryKey;autoIncrement" json:"id"`
+	Name      string         `gorm:"not null; type:varchar(255)" json:"name"`
+	Email     string         `gorm:"not null; unique; type:varchar(100); index" json:"email"`
+	Address   string         `gorm:"not null; unique; type:TEXT" json:"address"`
+	CreatedAt time.Time      `json:"createdAt" gorm:"index;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `json:"updatedAt" gorm:"index;default:CURRENT_TIMESTAMP"`
+	DeletedAt gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
+	UpdatedBy uint           `gorm:"not null; default: 0" json:"updatedBy"`
+	Invoices  []Invoice      `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"invoices"`
 }
