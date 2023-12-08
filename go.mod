c58d066df8f27db697899340e706aff99e2e5d7e
fajaramaulana <fajaragusmaulana@gmail.com>
2023-12-09 00:57:46 +0700

    add main.go

--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
 module esb-invoice
 
-go 1.21.4
+go 1.20.12
 
 require (
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/joho/godotenv v1.5.1
 	gorm.io/gorm v1.25.5
+)
+
+require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/driver/mysql v1.5.2
 )
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"esb-invoice/config/db"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+// @title ESB-Invoice
+// @version 1.0
+// @description ESB - Invoice
+// @termsOfService http://swagger.io/terms/
+// @contact.name Swagger API Team
+// @contact.url http://swagger.io
+// @contact.email fajaragusmaulana@gmail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @host localhost:8081
+
+func main() {
+	// Load environment variables
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+
+	// connect to database
+	dbInstance, err := db.ConnectionMysqlGorm()
+	if err != nil {
+		log.Fatal("Error connecting to the database:", err)
+	}
+
+	err = db.SeederDB(dbInstance)
+
+	if err != nil {
+		log.Fatal("Error Seeding DB:", err)
+	}
+
+}
