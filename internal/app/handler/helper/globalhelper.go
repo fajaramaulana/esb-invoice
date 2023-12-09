80ccd1116d9eb5700a0252400112fa75ea472c47
fajaramaulana <fajaragusmaulana@gmail.com>
2023-12-09 22:11:07 +0700

    Add ExtractFieldNameFromError function to globalhelper.go

--- internal/app/handler/helper/globalhelper.go
+++ internal/app/handler/helper/globalhelper.go
@@ -1,6 +1,10 @@
 package helper
 
-import "strconv"
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
 
 func ConvertStringToInt(str string) (int, error) {
 	stringConv, err := strconv.Atoi(str)
@@ -15,4 +19,29 @@ func MinLengthQueryParam(str string, min int) bool {
 		return true
 	}
 	return false
+}
+
+func ExtractFieldNameFromError(errorMessage string) (fieldErrorsReturn map[string]string) {
+	fieldErrors := make(map[string]string)
+	// Define a regular expression pattern to match the field name in the error message
+	regexPattern := `Key: '([^']+)' Error:Field validation for '([^']+)' failed on the '([^']+)' tag`
+	regex := regexp.MustCompile(regexPattern)
+
+	// Find all matches in the error message
+	matches := regex.FindAllStringSubmatch(errorMessage, -1)
+
+	// Check if a match is found
+
+	for _, match := range matches {
+		fieldName := match[2]
+		errorMessage := match[3]
+
+		// Combine the key and field name to form a unique identifier
+		identifier := fmt.Sprintf("%s", fieldName)
+
+		// Store the error message in the map using the identifier as the key
+		fieldErrors[identifier] = fmt.Sprintf("%s is %s", identifier, errorMessage)
+	}
+
+	return fieldErrors
 }
