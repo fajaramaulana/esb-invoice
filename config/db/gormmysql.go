5b8bef43bd04f2a713cdad4f2cc0d00666f2ce97
fajaramaulana <fajaragusmaulana@gmail.com>
2023-12-09 12:29:42 +0700

    add field on invoices table "payment_status"

--- config/db/gormmysql.go
+++ config/db/gormmysql.go
@@ -1,8 +1,10 @@
 package db
 
 import (
+	"errors"
 	"esb-invoice/server/repositories/schemas"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -10,23 +12,59 @@ import (
 	"gorm.io/gorm"
 )
 
+type Schema interface{}
+
 func ConnectionMysqlGorm() (*gorm.DB, error) {
 	err := godotenv.Load()
 
 	if err != nil {
+		log.Fatalf("Error load env %s", err)
 		return nil, err
 	}
 
 	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_DATABASE") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	fmt.Printf("%# v\n", dsn)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
+		log.Fatalf("Error connection database %s", err)
 		return nil, err
 	}
 
-	db.Debug().AutoMigrate(schemas.Customer{}, schemas.Item{}, schemas.Invoice{}, schemas.InvoiceItem{}, schemas.Type{})
+	if err := migrateProcess(db); err != "nil" {
+		log.Fatalf(err)
+		return nil, errors.New(err)
+	}
 
 	return db, nil
+}
+
+func migrateProcess(db *gorm.DB) string {
+	tableSchemas := map[string]Schema{
+		"customers":     &schemas.Customer{},
+		"items":         &schemas.Item{},
+		"invoices":      &schemas.Invoice{},
+		"invoice_items": &schemas.InvoiceItem{},
+		"types":         &schemas.Type{},
+	}
+
+	var errMessage string
+
+	for tableName, schema := range tableSchemas {
+		// check if table already exist
+		if exist := db.Migrator().HasTable(tableName); !exist {
+			// if table doesnt exist, migrate
+			if err := db.AutoMigrate(schema); err != nil {
+				log.Fatalf("Error migrating table %s: %s", tableName, err)
+				errMessage = fmt.Sprintf("Error migrating table %s: %s", tableName, err)
+			}
+			log.Printf("Table %s migrated successfully\n", tableName)
+			errMessage = "nil"
+		} else {
+			log.Printf("Table %s already exists\n", tableName)
+			errMessage = "nil"
+		}
+	}
+
+	return errMessage
 }
--- server/repositories/schemas/invoice.go
+++ server/repositories/schemas/invoice.go
@@ -7,18 +7,19 @@ import (
 )
 
 type Invoice struct {
-	InvoiceID   uint           `gorm:"primaryKey" json:"invoiceId"`
-	Subject     string         `gorm:"not null; type:varchar(255)" json:"subject"`
-	IssueDate   time.Time      `gorm:"not null" json:"issueDate"`
-	DueDate     time.Time      `gorm:"not null" json:"dueDate"`
-	CustomerID  uint           `gorm:"not null" json:"customerId" gorm:"index"`
-	Subtotal    float64        `gorm:"not null; type:decimal(10,2)" json:"subtotal"`
-	TaxRate     float64        `gorm:"not null; type:decimal(10,2)" json:"taxRate"`
-	TaxAmount   float64        `gorm:"not null; type:decimal(10,2)" json:"taxAmount"`
-	TotalAmount float64        `gorm:"not null; type:decimal(10,2)" json:"totalAmount"`
-	InvoiceItem InvoiceItem    `gorm:"foreignKey:InvoiceID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"invoiceItem"`
-	Customer    Customer       `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"customer"`
-	CreatedAt   time.Time      `json:"createdAt" gorm:"not null"`
-	UpdatedAt   time.Time      `json:"updatedAt" gorm:"not null, autoUpdateTime"`
-	DeletedAt   gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
+	InvoiceID     uint           `gorm:"primaryKey" json:"invoiceId"`
+	Subject       string         `gorm:"not null; type:varchar(255)" json:"subject"`
+	IssueDate     time.Time      `gorm:"not null" json:"issueDate"`
+	DueDate       time.Time      `gorm:"not null" json:"dueDate"`
+	CustomerID    uint           `gorm:"not null" json:"customerId" gorm:"index"`
+	PaymentStatus int            `gorm:"not null; comment:'1 = paid, 0 = unpaid'; type:int(1); default:0" json:"paymentStatus"`
+	Subtotal      float64        `gorm:"not null; type:decimal(10,2)" json:"subtotal"`
+	TaxRate       float64        `gorm:"not null; type:decimal(10,2)" json:"taxRate"`
+	TaxAmount     float64        `gorm:"not null; type:decimal(10,2)" json:"taxAmount"`
+	TotalAmount   float64        `gorm:"not null; type:decimal(10,2)" json:"totalAmount"`
+	InvoiceItem   InvoiceItem    `gorm:"foreignKey:InvoiceID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"invoiceItem"`
+	Customer      Customer       `gorm:"foreignKey:CustomerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"customer"`
+	CreatedAt     time.Time      `json:"createdAt" gorm:"not null"`
+	UpdatedAt     time.Time      `json:"updatedAt" gorm:"not null, autoUpdateTime"`
+	DeletedAt     gorm.DeletedAt `gorm:"index" json:"deletedAt,omitempty"`
 }
