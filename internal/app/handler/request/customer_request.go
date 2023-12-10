d5f848d40585052811ed8dda59e2550f1d4672bc
fajaramaulana <fajaragusmaulana@gmail.com>
2023-12-10 10:27:10 +0700

    Update JSON tags in customer and type request structs

--- internal/app/handler/request/customer_request.go
+++ internal/app/handler/request/customer_request.go
@@ -1,13 +1,13 @@
 package request
 
 type CreateCustomerRequest struct {
-	Name    string `form:"name" validate:"required,min=3,max=255" example:"Fajar Agus" binding:"required"`
-	Email   string `form:"email" validate:"required,email" example:"fajar@gmail.com" binding:"required"`
-	Address string `form:"address"  validate:"required,min=5" example:"Jl. Raya" binding:"required"`
+	Name    string `json:"name" form:"name" validate:"required,min=3,max=255" example:"Fajar Agus" binding:"required"`
+	Email   string `json:"email" form:"email" validate:"required,email" example:"fajar@gmail.com" binding:"required"`
+	Address string `json:"address" form:"address"  validate:"required,min=5" example:"Jl. Raya" binding:"required"`
 }
 
 type UpdateCustomerRequest struct {
 	Name    string `json:"name" form:"name" validate:"required,min=3,max=255" example:"Fajar Agus" binding:"required"`
 	Email   string `json:"email" form:"email" validate:"required,email" example:"fajar@gmail.com" binding:"required"`
-	Address string `json:"address"  form:"address"  validate:"required,min=5" example:"Jl. Raya" binding:"required"`
+	Address string `json:"address" form:"address"  validate:"required,min=5" example:"Jl. Raya" binding:"required"`
 }
--- internal/app/handler/request/type_request.go
+++ internal/app/handler/request/type_request.go
@@ -1,9 +1,9 @@
 package request
 
 type CreateTypeRequest struct {
-	Name string `form:"name" validate:"required,max=255" binding:"required" example:"Fajar Agus"`
+	Name string `json:"name" form:"name" validate:"required,max=255" binding:"required" example:"Fajar Agus"`
 }
 
 type UpdateTypeRequest struct {
-	Name string `form:"name" validate:"required,min=3,max=255" binding:"required" example:"Fajar Agus"`
+	Name string `json:"name" form:"name" validate:"required,min=3,max=255" binding:"required" example:"Fajar Agus"`
 }
